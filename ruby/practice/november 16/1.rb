741f5b57d2597962d6271ccf759c9721028a33ab
thuga-muffin <lubaburdette@gmail.com>
2016-11-02 00:00:12 -0400

    Tuesday 1 Nov 2016

---
+++ ruby/practice/november 16/1.rb
@@ -0,0 +1,36 @@
+favorite_book = nil
+puts favorite_book
+
+favorite_book ||= "Cat's Cradle"
+puts favorite_book
+
+favorite_book ||= "Why's (Poignant) Guide to Ruby"
+puts favorite_book
+
+favorite_book = "Why's (Poignant) Guide to Ruby"
+puts favorite_book
+
+
+
+# ############################
+
+def multiple_of_three(n)
+   n % 3 == 0 ? "True" : "False"
+end
+
+####################################
+
+
+def a
+  puts "A was evaluated!"
+  return true
+end
+
+def b
+  puts "B was also evaluated!"
+  return true
+end
+
+puts a || b
+puts "------"
+puts a && b
