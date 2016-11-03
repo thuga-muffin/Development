5e8ed8005455688d5b93743f05abad3e786c9581
thuga-muffin <lubaburdette@gmail.com>
2016-11-03 00:09:48 -0400

    2 nov 16

---
+++ ruby/practice/november 16/2.rb
@@ -0,0 +1,12 @@
+my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for my_array.each do |x|
+	if x % 2 = 0
+		puts x
+end
+	
+
+#######################################
+
+
+"P".upto("L") { |n| puts letter, " " }
