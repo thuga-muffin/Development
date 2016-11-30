f5e882de9dac325162d5fc10a74adf431a0aed83
thuga-muffin <lubaburdette@gmail.com>
2016-11-29 23:30:09 -0500

    still working on that one line
    Trying to write logic that detects numerical characters in a string.

--- ruby/ex35.rb
+++ ruby/ex35.rb
@@ -86,6 +86,31 @@ end # start
 
 start
       
+
+# module Gold_room
+
+#     def gold_room
+#     puts "This room is full of gold. How much do you take?"
+
+#     print "> "
+#     choice = $stdin.gets.chomp
+
+#     if choice.include?("0") || choice.include?("1")
+#       how_much = choice.to_i
+#     else
+#       dead("Man, learn how to type a number.")
+#     end
+
+#     if how_much < 50
+#       puts "Nice, you're not greedy, you win!"
+#       exit(0)
+#     else
+#       dead("You greedy bastard!")
+#     end
+
+#   end # gold_room
+
+# end
       
       
         
